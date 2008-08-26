fc7ca3b62f7be7aeb7d17172ea44318c0ad397a2
David Reuss <davp@tv2.dk>
2008-08-26 13:15:45 +0200

    Polished first-time setup once again
    Makes better use of git config, to store all gitex related variables in globally
    
    This makes it a lot easier to install
    
    Should essentially be:
    
    1. clone project
    2. edit setup.sh to fit your environment, and run it
    3. PROFIT!

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-die() {
-    echo "$@" >&2
-    exit 1
-}
+# Change these values to whatever fits your preferences
 
+gitex_home="$HOME/.gitex" # Where gitex will live and all imported/exported repos as well
+gitex_group=coders # what shared group will your repositories be writable by?
+gitex_cvsroot="$CVSROOT" # The CVS root of your cvs repsoitories - defaults to $CVSROOT
+gitex_lastexporttag=GITEX_LAST_EXPORT # At every export gitex will tag your commit by this name
+                                      # Makes it easy to find out what was last exported
 
-test -n "$gitex_shared_group" || die "Configuration not setup. Aborting"
-test -d "$1" || die "Directory $1 not found. Aborting"
+################################
+# Do not edit below this block #
+################################
 
-# gitty first time setup
-cd "$1"
-
-mkdir cvs git
-chgrp -R "$gitex_shared_group" cvs git
+git config --global gitex.home "$gitex_home"
+git config --global gitex.group "$gitex_group"
+git config --global gitex.cvsroot "$gitex_cvsroot"
+git config --global gitex.lastexporttag "$gitex_lastexporttag"
